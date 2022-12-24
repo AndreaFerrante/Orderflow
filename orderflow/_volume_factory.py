8b5dd361eed6d1e561c8f3cac84400a847c5a2db
Andrea Ferrante <andreaferrante1986@gmail.com>
2022-12-24 21:58:03 +0100

    Cleaned service factory

--- orderflow/_volume_factory.py
+++ orderflow/_volume_factory.py
@@ -1,15 +1,26 @@
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-def get_ticker_in_folder(path:str='/Users/andrea/Downloads/Data_CME/', ticker:str='ES', cols:list=None):
+def get_ticker_in_folder(path:str, ticker:str='ES', cols:list=None) -> pd.DataFrame:
+
+    '''
+    Given a path and a ticker sign, this functions read all file in it starting with the ticker symbol (e.g. ES)
+    :param path: path to ticker data to read
+    :param ticker: ticker to read in the form of ES, ZN, ZB . . .
+    :param cols: columns to import...
+    :return: DataFrame of all read ticker files
+    '''
 
     files   = os.listdir( path )
     stacked = []
     for file in tqdm.tqdm(files):
+
         if file.startswith(ticker):
+
             print(f'Reading file named {file} ...')
             if cols is None:
                 stacked.append( pd.read_csv( os.path.join(path, file), sep=';') )
@@ -18,40 +29,40 @@ def get_ticker_in_folder(path:str='/Users/andrea/Downloads/Data_CME/', ticker:st
 
     return pd.concat( stacked )
 
-def half_hour(x):
-    if x >= 30:
-        return "30"
-    else:
-        return "00"
 
-data_path = '/Users/andrea/Downloads/Data_CME/'
-data      = 'ESZ22-CME_20221104_155959.txt'
+def plot_half_hour_volume(data_path:str, data_name:str):
 
-all_data     = get_ticker_in_folder( data_path, 'ES', cols=['Date', 'Time', 'Volume'] )
-all_data     = all_data.assign(Hour     = all_data.Time.str[:2].astype(str))
-all_data     = all_data.assign(Minute   = all_data.Time.str[3:5].astype(int))
-all_data     = all_data.assign(HalfHour = all_data.Hour.str.zfill(2) + all_data.Minute.apply(half_hour))
-all_data_gb  = all_data.groupby(['Date', 'HalfHour']).agg({'Volume':'sum'}).reset_index()
-all_data_gb_ = all_data.groupby(['HalfHour']).agg({'Volume':'sum'}).reset_index()
+    '''
+    This function helps understanding the "volume smile" so that the peak in volume given hal hours is the market open
+    :param data_path: file system path to the data
+    :param data_name: file name to import
+    :return: a plot in matplotlib with bars per half-hour (the bigger counting bar is the one that finds market opens)
+    '''
 
-es = pd.read_csv( os.path.join(data_path, data), sep=';' )
-es = es[es.Price != 0]
-es = es.assign(Index    = np.arange(0, es.shape[0], 1))
-es = es.assign(Hour     = es.Time.str[:2].astype(str))
-es = es.assign(Minute   = es.Time.str[3:5].astype(int))
-es = es.assign(HalfHour = es.Hour.str.zfill(2) + es.Minute.apply(half_hour))
+    def half_hour(x):
+        if x >= 30:
+            return "30"
+        else:
+            return "00"
 
-max_volume = es.groupby(['HalfHour']).agg({'Volume':'sum'}).reset_index()
-plt.bar(max_volume.HalfHour, max_volume.Volume)
-plt.xlabel('HalfHour')
-plt.ylabel('Volume')
-plt.xticks(rotation=90)
-plt.tight_layout()
+    try:
+        es = pd.read_csv( os.path.join(data_path, data_name), sep=';' )
+    except:
+        es = pd.read_csv(os.path.join(data_path, data_name), sep=',')
 
-red   = es[(es.Volume >= 10) & (es.TradeType == 1 ) & (es.TotalAskDepth < es.TotalBidDepth ) & (es.BidSize >= 300)] # & (es.Hour < 16)]
-green = es[(es.Volume >= 10) & (es.TradeType == 2 ) & (es.TotalAskDepth > es.TotalBidDepth ) & (es.AskSize >= 300)] # & (es.Hour < 16)]
+    es = es[es.Price != 0] # Remove recording impurities...
+    es = es.assign(Index    = np.arange(0, es.shape[0], 1))  # Set an index fro reference plotting...
+    es = es.assign(Hour     = es.Time.str[:2].astype(str))   # Extract the hour...
+    es = es.assign(Minute   = es.Time.str[3:5].astype(int))  # Extract the minute...
+    es = es.assign(HalfHour = es.Hour.str.zfill(2) + es.Minute.apply(half_hour)) # Identifies half hours...
 
-plt.plot(es.Index, es.Price, zorder=0, lw=0.7)
-plt.scatter(red.Index, red.Price, color='red', zorder=1, edgecolors='black')
-plt.scatter(green.Index, green.Price, color='green', zorder=1, edgecolors='black')
-plt.show()
+    # Plot bar chart in which the
+    max_volume = es.groupby(['HalfHour']).agg({'Volume':'sum'}).reset_index()
+    plt.bar(max_volume.HalfHour, max_volume.Volume)
+    plt.xlabel('HalfHour')
+    plt.ylabel('Volume')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+
+
+

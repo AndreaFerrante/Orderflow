e81b13c441d73cb9ec494cc1883b0bc3bf5095aa
Andrea Ferrante <andreaferrante1986@gmail.com>
2022-12-25 09:51:53 +0100

    Added modification to half hour plotter

--- orderflow/_volume_factory.py
+++ orderflow/_volume_factory.py
@@ -13,15 +13,14 @@ def half_hour(x):
         return "00"
 
 
-def prepare_data(data:pd.DataFrame) -> pd.DataFrame:
+def prepare_data(data: pd.DataFrame) -> pd.DataFrame:
 
-    '''
+    """
     Given usual data recorded, this function returns it corrected since its CSV recoding so it adds pandas datatypes
     fro data reshaping and data plotting
     :param data: given usual data recorded
     :return: dataframe corrected
-    '''
-
+    """
 
     # es = es.assign(Index    = np.arange(0, es.shape[0], 1))  # Set an index fro reference plotting...
     # es = es.assign(Hour     = es.Time.str[:2].astype(str))   # Extract the hour...
@@ -29,115 +28,124 @@ def prepare_data(data:pd.DataFrame) -> pd.DataFrame:
     # es = es.assign(HalfHour = es.Hour.str.zfill(2) + es.Minute.apply(half_hour)) # Identifies half hours...
 
     data = (
-            data.
-            assign(Index       = np.arange(0, data.shape[0], 1)).
-            assign(Hour        = data.Time.str[:2].astype(str)).
-            assign(Minute      = data.Time.str[3:5].astype(int)).
-            assign(HalfHour    = data.Hour.str.zfill(2) + data.Minute.apply(half_hour)).
-            assign(DateTime    = data.Date.astype(str) + ' ' + data.Time.astype(str)).
-            assign(DateTime_TS = pd.to_datetime( data.DateTime ))
-            )
+        data.assign(Index=np.arange(0, data.shape[0], 1))
+        .assign(Hour=data.Time.str[:2].astype(str))
+        .assign(Minute=data.Time.str[3:5].astype(int))
+        .assign(HalfHour=data.Hour.str.zfill(2) + data.Minute.apply(half_hour))
+        .assign(DateTime=data.Date.astype(str) + " " + data.Time.astype(str))
+        .assign(DateTime_TS=pd.to_datetime(data.DateTime))
+    )
 
     return data
 
 
-def get_tickers_in_folder(path:str, ticker:str='ES', cols:list=None, break_at:int=99999) -> pd.DataFrame:
+def get_tickers_in_folder(
+    path: str, ticker: str = "ES", cols: list = None, break_at: int = 99999
+) -> pd.DataFrame:
 
-    '''
+    """
     Given a path and a ticker sign, this functions read all file in it starting with the ticker symbol (e.g. ES)
     :param path: path to ticker data to read
     :param ticker: ticker to read in the form of ES, ZN, ZB . . .
     :param cols: columns to import...
     :param break_at: how many ticker files to read ?
     :return: DataFrame of all read ticker files
-    '''
+    """
 
-    files   = os.listdir( path )
+    files = os.listdir(path)
     stacked = []
     counter = 0
     for file in tqdm.tqdm(files):
 
         if file.startswith(ticker):
 
-            print(f'Reading file named {file} ...')
+            print(f"Reading file named {file} ...")
             if cols is None:
-                stacked.append( pd.read_csv( os.path.join(path, file), sep=';') )
+                stacked.append(pd.read_csv(os.path.join(path, file), sep=";"))
             else:
-                stacked.append(pd.read_csv(os.path.join(path, file), sep=';', usecols=cols))
+                stacked.append(
+                    pd.read_csv(os.path.join(path, file), sep=";", usecols=cols)
+                )
 
             counter += 1
             if counter >= break_at:
-                return pd.concat( stacked )
+                return pd.concat(stacked)
 
-    return pd.concat( stacked )
+    return pd.concat(stacked)
 
 
-def plot_half_hour_volume(data_already_read:bool, data:pd.DataFrame, data_path:str='', data_name:str='') -> None:
+def plot_half_hour_volume(
+    data_already_read: bool,
+    data: pd.DataFrame,
+    data_path: str = "",
+    data_name: str = "",
+) -> None:
 
-    '''
+    """
     This function helps understanding the "volume smile" so that the peak in volume given hal hours is the market open
     :param data_path: file system path to the data
     :param data_name: file name to import
     :return: a plot in matplotlib with bars per half-hour (the bigger counting bar is the one that finds market opens)
-    '''
+    """
 
     if not data_already_read:
         try:
-            data = pd.read_csv( os.path.join(data_path, data_name), sep=';' )
+            data = pd.read_csv(os.path.join(data_path, data_name), sep=";")
         except:
-            data = pd.read_csv(os.path.join(data_path, data_name), sep=',')
+            data = pd.read_csv(os.path.join(data_path, data_name), sep=",")
     else:
         data = data
 
-    data = data[data.Price != 0] # Remove recording impurities...
-    data = data.assign(Index    = np.arange(0, data.shape[0], 1))  # Set an index fro reference plotting...
-    data = data.assign(Hour     = data.Time.str[:2].astype(str))   # Extract the hour...
-    data = data.assign(Minute   = data.Time.str[3:5].astype(int))  # Extract the minute...
-    data = data.assign(HalfHour = data.Hour.str.zfill(2) + data.Minute.apply(half_hour)) # Identifies half hours...
+    data = data[data.Price != 0]  # Remove recording impurities...
+    data = data.assign(
+        Index=np.arange(0, data.shape[0], 1)
+    )  # Set an index fro reference plotting...
+    data = data.assign(Hour=data.Time.str[:2].astype(str))  # Extract the hour...
+    data = data.assign(Minute=data.Time.str[3:5].astype(int))  # Extract the minute...
+    data = data.assign(
+        HalfHour=data.Hour.str.zfill(2) + data.Minute.apply(half_hour)
+    )  # Identifies half hours...
 
     # Plot bar chart in which the
-    max_volume = data.groupby(['HalfHour']).agg({'Volume':'sum'}).reset_index()
+    max_volume = data.groupby(["HalfHour"]).agg({"Volume": "sum"}).reset_index()
     plt.bar(max_volume.HalfHour, max_volume.Volume)
-    plt.xlabel('HalfHour')
-    plt.ylabel('Volume')
+    plt.xlabel("HalfHour")
+    plt.ylabel("Volume")
     plt.xticks(rotation=90)
     plt.tight_layout()
 
 
-def filter_big_prints_on_ask(data:pd.DataFrame, volume_filter:int=100) -> pd.DataFrame:
+def filter_big_prints_on_ask(
+    data: pd.DataFrame, volume_filter: int = 100
+) -> pd.DataFrame:
 
-    '''
+    """
     Given the canonical dataframe recorded, this functions returns filtered volume dataframe on the ASK
     :param data: canonical dataframe recorded
     :param volume_filter: time and sales dataframe recorded volume filter
     :return: dataframe with the given filter
-    '''
+    """
 
-    filtered_on_ask = (
-                       data.
-                       query('TradeType == 2').
-                       query('Volume >= ' + str(volume_filter) + '')
-                      )
+    filtered_on_ask = data.query("TradeType == 2").query(
+        "Volume >= " + str(volume_filter) + ""
+    )
 
     return filtered_on_ask
 
 
-def filter_big_prints_on_bid(data: pd.DataFrame, volume_filter: int=100) -> pd.DataFrame:
+def filter_big_prints_on_bid(
+    data: pd.DataFrame, volume_filter: int = 100
+) -> pd.DataFrame:
 
-    '''
+    """
     Given the canonical dataframe recorded, this functions returns filtered volume dataframe on the BID
     :param data: canonical dataframe recorded
     :param volume_filter: time and sales dataframe recorded volume filter
     :return: dataframe with the given filter
-    '''
+    """
 
-    filtered_on_bid = (
-                        data.
-                            query('TradeType == 1').
-                            query('Volume >= ' + str(volume_filter) + '')
-                      )
+    filtered_on_bid = data.query("TradeType == 1").query(
+        "Volume >= " + str(volume_filter) + ""
+    )
 
     return filtered_on_bid
-
-
-
--- orderflow/volume_profile.py
+++ orderflow/volume_profile.py
@@ -4,32 +4,32 @@ import operator
 from tqdm import tqdm
 
 
-def get_dynamic_vp(df:pd.DataFrame) -> pd.DataFrame:
+def get_dynamic_vp(df: pd.DataFrame) -> pd.DataFrame:
 
-    '''
+    """
     Given the canonical dataframe recorded, this function returns a dataframe with volume in ASK / BID restarted at the
     of a given day.
     :param df: canonical dataframe recorded (tick-by-tick wth the DOM attached)
     :return: dataframe of
-    '''
+    """
 
-    volume    = np.array(df.Volume)
-    price     = np.array(df.Price)
-    side      = np.array(df.TradeType)
-    sday      = np.array(df.Index_DayShift)
+    volume = np.array(df.Volume)
+    price = np.array(df.Price)
+    side = np.array(df.TradeType)
+    sday = np.array(df.Index_DayShift)
 
-    volume_a  = {}
-    volume_b  = {}
+    volume_a = {}
+    volume_b = {}
     len_final = df.shape[0]
-    voa       = np.zeros(len_final)
-    vob       = np.zeros(len_final)
+    voa = np.zeros(len_final)
+    vob = np.zeros(len_final)
 
     if side[0] == 2:
-        voa[0]             = volume[0]
+        voa[0] = volume[0]
         volume_a[price[0]] = volume[0]
         volume_b[price[0]] = 0
     elif side[0] == 1:
-        vob[0]             = volume[0]
+        vob[0] = volume[0]
         volume_a[price[0]] = 0
         volume_b[price[0]] = volume[0]
 
@@ -67,7 +67,7 @@ def get_dynamic_vp(df:pd.DataFrame) -> pd.DataFrame:
                     except Exception as ex:
                         print(ex)
             else:
-                print('no side')
+                print("no side")
         else:
             volume_a = {}
             volume_b = {}
@@ -78,7 +78,7 @@ def get_dynamic_vp(df:pd.DataFrame) -> pd.DataFrame:
                 volume_b[price[idx]] = volume[idx]
                 vob[idx] = volume_b[price[idx]]
             else:
-                print('no side')
+                print("no side")
 
     df = df.assign(VP_Ask=pd.Series(voa, dtype=int).reset_index(drop=True))
     df = df.assign(VP_Bid=pd.Series(vob, dtype=int).reset_index(drop=True))
@@ -86,39 +86,38 @@ def get_dynamic_vp(df:pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def get_daily_moving_POC(df:pd.DataFrame) -> np.array:
+def get_daily_moving_POC(df: pd.DataFrame) -> np.array:
 
-    '''
+    """
     Given the canonical dataframe recorded, this function returns the Point of Control (i.e. POC) that is moving during
     the day giving the volume sentiment of uptrending market or choppy market of downtrending market.
     :param df: anonical dataframe recorded
     :return: numpy array for the daily moving poc
-    '''
+    """
 
-    volume    = np.array(df.Volume)
-    price     = np.array(df.Price)
-    date      = np.array(df.Date)
+    volume = np.array(df.Volume)
+    price = np.array(df.Price)
+    date = np.array(df.Date)
     poc_final = {}
-    len_      = len(price)
-    poc_      = np.zeros(len_)
+    len_ = len(price)
+    poc_ = np.zeros(len_)
 
     poc_final[price[0]] = volume[0]
-    poc_[0]             = price[0]
+    poc_[0] = price[0]
 
     for i in tqdm(range(1, len_ - 1)):
         cp = price[i]
-        if date[i] != date[i-1]:
+        if date[i] != date[i - 1]:
             poc_final.clear()
-            poc_final[ cp ] = volume[i]
-            poc_[i]         = cp
+            poc_final[cp] = volume[i]
+            poc_[i] = cp
         else:
             if cp in poc_final:
-                poc_final[ cp ] += volume[i]
+                poc_final[cp] += volume[i]
             else:
-                poc_final[ cp ] = volume[i]
+                poc_final[cp] = volume[i]
             poc_[i] = max(poc_final.items(), key=operator.itemgetter(1))[0]
 
-    poc_[len_-1] = price[len_-1]
+    poc_[len_ - 1] = price[len_ - 1]
 
     return poc_
-

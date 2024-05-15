9a75d21dddb5100639cd4989ae48a2c3f45e36bf
Andrea <andreaferrante1986@gmail.com>
2024-05-15 22:35:07 +0200

    Added small stats c hanges

--- orderflow/stats.py
+++ orderflow/stats.py
@@ -13,8 +13,8 @@ from Orderflow.orderflow._volume_factory import get_tickers_in_folder
 
 
 ticker = get_tickers_in_folder(ticker         = "ZN",
-                               future_letters = ['H', 'M', 'U', 'Z'],
-                               year           = 23,
+                               future_letters = ['M'],
+                               year           = 24,
                                market         = "CBOT",
                                path           = get_current_os())
 
@@ -27,16 +27,26 @@ ticker_gb = (ticker.
 ticker_gb
 
 
-pre_market  = ticker.filter(pl.col("Datetime").dt.time() <=  time(8, 30, 00))
+pre_market  = ticker.filter((pl.col("Datetime").dt.time() > time(7, 30, 00)) &
+                            (pl.col("Datetime").dt.time() < time(8, 30, 00)))
 post_market = ticker.filter((pl.col("Datetime").dt.time() >  time(8, 30, 00)) &
-                            (pl.col("Datetime").dt.time() <= time(11, 00, 00)))
+                            (pl.col("Datetime").dt.time() < time(10, 30, 00)))
 
 pre_market_gb  = (pre_market.
                   group_by(['Date']).
-                  agg([pl.col('Volume').sum().alias('VolSumPre')]))
+                  agg([pl.col('Volume').sum().alias('VolSumPre'),
+                       pl.col('Price').max().alias('PriceMaxPre'),
+                       pl.col('Price').min().alias('PriceMinPre')])).sort('Date')
+pre_market_gb  = pre_market_gb.with_columns(PrePriceDelta     = pre_market_gb['PriceMaxPre'] - pre_market_gb['PriceMinPre'])
+pre_market_gb  = pre_market_gb.with_columns(VolPricePreRatio  = (pre_market_gb['PrePriceDelta'] / pre_market_gb['VolSumPre']) * 1_000_000)
+
 post_market_gb = (post_market.
                   group_by(['Date']).
-                  agg([pl.col('Volume').sum().alias('VolSumPost')]))
+                  agg([pl.col('Volume').sum().alias('VolSumPost'),
+                       pl.col('Price').max().alias('PriceMaxPost'),
+                       pl.col('Price').min().alias('PriceMinPost')])).sort('Date')
+post_market_gb = post_market_gb.with_columns(PostPriceDelta    = post_market_gb['PriceMaxPost'] - post_market_gb['PriceMinPost'])
+post_market_gb = post_market_gb.with_columns(VolPricePostRatio = (post_market_gb['PostPriceDelta'] / post_market_gb['VolSumPost']) * 1_000_000)
 
 merged = pre_market_gb.join(
     post_market_gb,
@@ -44,7 +54,9 @@ merged = pre_market_gb.join(
     on  = 'Date'
 ).drop_nulls()
 
-np.corrcoef(merged['VolSumPre'], merged['VolSumPost'])
+##############################################################################
+np.corrcoef(merged['VolPricePreRatio'], merged['VolPricePostRatio'])
+##############################################################################
 
 
 # print_med_big     = ticker.filter( pl.col('Volume') >= 1000 )
@@ -55,5 +67,9 @@ np.corrcoef(merged['VolSumPre'], merged['VolSumPost'])
 # plt.plot(ticker['Datetime'], ticker['Price'], zorder=0)
 # plt.scatter(print_med_big_ask['Datetime'], print_med_big_ask['Price'], zorder=1, s=50, edgecolor='black', color='lime')
 # plt.scatter(print_med_big_bid['Datetime'], print_med_big_bid['Price'], zorder=1, s=50, edgecolor='black', color='red')
+
+
+
+
 
 
